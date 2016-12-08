89114f98e49718f60cc42610de2facb93cd439f0
Chikara Matsuda <matsudachikara@yahoo.co.jp>
2016-12-09 00:33:07 +0900

    dynamo用migrationファイル追加

---
+++ db/migrate/20161208152530_create_csv_settings.rb
@@ -0,0 +1,34 @@
+class CreateCsvSettings < ActiveRecord::Migration[5.0]
+  def up
+    unless exists? 'csv_settings'
+      table = dynamo_db.tables.create('csv_settings',
+                                      1, 1, :hash_key => { :csv_settind_id => :number })
+      while table.status == :creating
+        puts "Waiting for table create to complete.."
+        sleep 2
+      end
+    end
+  end
+
+  def down
+    if exists? 'csv_settings'
+      table = dynamo_db.tables['csv_settings']
+      table.delete
+
+      while exists? 'csv_settings'
+        puts "Waiting for table delete to complete.."
+        sleep 2
+      end
+    end
+  end
+
+  private
+
+  def exists?(name)
+    dynamo_db.tables['csv_settings'].exists?
+  end
+
+  def dynamo_db
+    @dynamo_db ||= AWS::DynamoDB.new
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207140821) do
+ActiveRecord::Schema.define(version: 20161208152530) do
 
   create_table "mysql_csv_settings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
